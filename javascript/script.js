447db68c26ddf6edeb0f3179d45857e3148644b6
Lorena Ramirez <ramirezlorenaa18@gmail.com>
2020-11-05 23:13:34 -0500

    Added timer function to countdown when user clicks start

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,12 @@
 var homeDisplay = document.querySelector("#home-page");
 var startBtn = document.querySelector("#start");
 var scoreBtn = document.querySelector(".nav-item");
+var secondsLeft = 120;
+var timerEl = document.createElement("h3");
+timerEl.textContent = "Timer: " + secondsLeft;
+document.body.appendChild(timerEl);
+var secondsLeft = 120;
+
 //Hide selected content on page
 function toggleDisplay(content){
     if (content.style.display === "none") {
@@ -10,9 +16,21 @@ function toggleDisplay(content){
       }
 }
 
+function timer(){
+var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timerEl.textContent = "Timer: " + secondsLeft;
+
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+    }
+
+  }, 1000);
+}
+
 function startQuiz(){
     toggleDisplay(homeDisplay);
-    
+    timer();
   
 }
 
